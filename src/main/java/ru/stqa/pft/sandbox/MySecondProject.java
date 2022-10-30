db2cc127bdbcc516f8d922f8a15271c737fc1154
aibukrid <115988571+aibukrid@users.noreply.github.com>
2022-10-30 22:58:02 +0300

    Операции с целыми числами

--- src/main/java/ru/stqa/pft/sandbox/MySecondProject.java
+++ src/main/java/ru/stqa/pft/sandbox/MySecondProject.java
@@ -3,5 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MySecondProject {
     public static void main(String[] args) {
         System.out.println("Hello Radik!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
     }
 }
