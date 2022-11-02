413713a8f3aa03fec31e41e696ff65e5652d49da
aibukrid <115988571+aibukrid@users.noreply.github.com>
2022-11-02 18:32:13 +0300

    Для множественных приветственных сообщений: использование значения somebody, как параметр, для функции hello

--- src/main/java/ru/stqa/pft/sandbox/MySecondProject.java
+++ src/main/java/ru/stqa/pft/sandbox/MySecondProject.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.sandbox;
 
 public class MySecondProject {
-    public static void main(String[] args) {
-        String somebody = "Radik";
-        System.out.println("Hello " + somebody + "!");
 
-        double l = 8.0;
-        double s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + (l * l));
+    public static void main(String[] args) {
+        hello("Radik");
+        hello("user");
+        hello("Alexei");
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
     }
 }
