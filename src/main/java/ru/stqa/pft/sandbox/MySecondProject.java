376394c0dde9a19ac4007da6c6e7c0de388fc69b
aibukrid <115988571+aibukrid@users.noreply.github.com>
2022-11-02 17:48:12 +0300

    Удалить

--- src/main/java/ru/stqa/pft/sandbox/MySecondProject.java
+++ src/main/java/ru/stqa/pft/sandbox/MySecondProject.java
@@ -4,26 +4,6 @@ public class MySecondProject {
     public static void main(String[] args) {
         System.out.println("Hello Radik!");
 
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
-
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-        System.out.println(2.0 / 2);
-
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-        System.out.println(2 + "2");
-
-        System.out.println(2 + 2 * 2);
-        System.out.println((2 + 2) * 2);
-
-        System.out.println("2 + 2 = " + 2 + 2);
-        System.out.println("2 + 2 = " + (2 + 2));
-
         System.out.println("Площадь квадрата со стороной " + 8 + " = " + (8 * 8));
 
         int l = 8;
